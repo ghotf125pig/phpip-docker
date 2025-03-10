15d54c567616540eef8b8d7124e91121b059459f
Nate Robinson <nate@lunamicro.se>
2025-03-10 20:27:53 +0100

    Fix mysql hostname to match service in docker-compose

--- app/init_db.php
+++ app/init_db.php
@@ -1,5 +1,5 @@
 <?php
-$connection = new PDO('mysql:host=mysql2;dbname=mysql;charset=utf8', 'root', 'super-secret-password');
+$connection = new PDO('mysql:host=mysql;dbname=mysql;charset=utf8', 'root', 'super-secret-password');
 $connection->exec( "CREATE DATABASE IF NOT EXISTS phpip DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;");
 $connection->exec( "CREATE USER IF NOT EXISTS phpip@localhost IDENTIFIED BY 'phpip';");
 $connection->exec( "GRANT ALL PRIVILEGES ON phpip.* TO phpip@localhost;");
