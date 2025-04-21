5af959937995ff0342f54d68f3a67081f50131f1
朱绍伟 <zhushaowei@rivergame.net>
2025-04-21 15:31:36 +0800

    debug

--- app/install-phpip-docker.sh
+++ app/install-phpip-docker.sh
@@ -20,7 +20,8 @@ echo "
 ********************************
 Installing dependencies
 ********************************"
-composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
+#composer config -g repo.packagist composer https://mirrors.aliyun.com/composer/
+composer config -g repo.packagist composer https://mirrors.cloud.tencent.com/composer/
 composer install
 php artisan key:generate
 php artisan config:clear
